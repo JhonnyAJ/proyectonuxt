461869b0737b4fc8e74fd3bfcd9fdf203fbd1005
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-09-30 18:44:54 -0600

    Add files via upload

--- pages/edificios_index.vue
+++ pages/edificios_index.vue
@@ -17,7 +17,7 @@
    <h3 style="margin-top: 15px">Edificios</h3>
      <p>Esta sección presenta los edificios y sus caracteristicas</p>
 <div class="row">
-<div class="card" style="width: 18rem;" v-for="edificio of edificios" :key="edificio.slug">
+<div class="card" style="width: 18rem; margin: 10px" v-for="edificio of edificios" :key="edificio.slug">
   <div class="card-body">
     <h5 class="card-title">{{edificio.title}}</h5>
     <NuxtLink :to="{ name: 'edificios-slug', params: { slug: edificio.slug } }">Ver edificio</NuxtLink>
