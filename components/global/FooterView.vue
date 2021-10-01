df14a32d29a7a2efe88a6e50e37a318b05811f06
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-09-30 19:41:03 -0600

    Add files via upload

--- components/global/FooterView.vue
+++ components/global/FooterView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row"  style="margin-top: 15px">
-    <button disabled="disabled" class="twelve columns button-primary">
+    <button disabled="disabled" class="twelve columns button-dark">
       Copyright (c) 2021 - UNA
     </button>
   </div>
