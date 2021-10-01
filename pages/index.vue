813eff216e42065dd574531e6927f1556b4a286c
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-09-30 18:13:02 -0600

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,13 @@
 <template>
  <div class="container">
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-   <HeaderView />
-   <h2 style="margin-top: 15px">Computer Science Books</h2>
-	  <p>This website offers information about highest buildings.</p>
-    <button class="btn btn-primary">Prueba</button>
-    <ul>
-	  <li> <NuxtLink to="/edificios_index">Buildings information</NuxtLink></li>
-	  <li> <NuxtLink to="/arquitectos_index">Architects information</NuxtLink></li>
-	  <li> <NuxtLink to="/ciudades_index">Cities information</NuxtLink></li>
-	</ul>
+   
+   <h2 style="margin-top: 15px">Edificios altos y dónde encontrarlos</h2>
+	  <p>Impresionáte con nuestro catálogo de edificio</p>
+    <HeaderView />
    <FooterView />
  </div>
-</template>
+</template> 
 
 <script>
 export default {
