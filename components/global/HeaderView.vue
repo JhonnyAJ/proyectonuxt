35a1f2fe7260e57839ddcbb3f84ece21b967b569
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-09-30 19:33:04 -0600

    Add files via upload

--- components/global/HeaderView.vue
+++ components/global/HeaderView.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="row"  style="margin-top: 15px">
-      <NuxtLink class="three columns button button-primary" to="/">Inicio</NuxtLink>
-      <NuxtLink class="three columns button button-primary" to="/edificios_index">Edificios</NuxtLink>
-      <NuxtLink class="three columns button button-primary" to="/arquitectos_index">Arquitectos</NuxtLink>
-      <NuxtLink class="three columns button button-primary" to="/ciudades_index">Ciudades</NuxtLink>
+      <NuxtLink class="three columns button button-dark" to="/">Inicio</NuxtLink>
+      <NuxtLink class="three columns button button-dark" to="/edificios_index">Edificios</NuxtLink>
+      <NuxtLink class="three columns button button-dark" to="/arquitectos_index">Arquitectos</NuxtLink>
+      <NuxtLink class="three columns button button-dark" to="/ciudades_index">Ciudades</NuxtLink>
     </div>
 </template>
