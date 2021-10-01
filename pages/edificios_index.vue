59dae90a26f2c7b99be9da94ed7ef6dce3d35b11
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-09-30 18:29:31 -0600

    Add files via upload

--- pages/edificios_index.vue
+++ pages/edificios_index.vue
@@ -11,14 +11,22 @@
 
 <template>
   <div class="container">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   
    <HeaderView />
-   <h3 style="margin-top: 15px">Buildings Information</h3>
-     <p>This section presents information about buildings</p>
-   <ul>
+   <h3 style="margin-top: 15px">Edificios</h3>
+     <p>Esta sección presenta los edificios y sus caracteristicas</p>
+
+     <div class="card" v-for="edificio of edificios" :key="edificio.slug">
+       <div class="card-body">
+         <NuxtLink :to="{ name: 'edificios-slug', params: { slug: edificio.slug } }">{{edificio.title}}</NuxtLink>
+       </div>
+     </div>
+   <!-- <ul>
      <li v-for="edificio of edificios" :key="edificio.slug">
        <NuxtLink :to="{ name: 'edificios-slug', params: { slug: edificio.slug } }">{{edificio.title}}</NuxtLink>
      </li>
-   </ul>
+   </ul> -->
    <FooterView />
  </div>
 </template>
