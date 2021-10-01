1f45b4c32b1816795377a7d5fe9475216dd46b9f
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-09-30 19:10:24 -0600

    Add files via upload

--- pages/ciudades_index.vue
+++ pages/ciudades_index.vue
@@ -11,6 +11,8 @@
 
 <template>
   <div class="container">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
    <HeaderView />
    <h3 style="margin-top: 15px">Ciudades</h3>
      <p>Investiga sobre las ciudades que albergan estos edificios</p>
