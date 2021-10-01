07cef760f795ad79cda6d8679bf77a611abf82a4
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-09-30 18:03:50 -0600

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
  <div class="container">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
    <HeaderView />
    <h2 style="margin-top: 15px">Computer Science Books</h2>
 	  <p>This website offers information about highest buildings.</p>
+    <button class="btn btn-primary">Prueba</button>
     <ul>
 	  <li> <NuxtLink to="/edificios_index">Buildings information</NuxtLink></li>
 	  <li> <NuxtLink to="/arquitectos_index">Architects information</NuxtLink></li>
